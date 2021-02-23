9d0874f1250a55e1a69f2d9a0afc9c99b7b860e0
Ben Martin <ben@brmartin.co.uk>
2021-02-23 13:59:33 +0000

    Create User type

--- Solution/booking_app/Model.java
+++ Solution/booking_app/Model.java
@@ -10,4 +10,22 @@ public class Model {
         return DriverManager.getConnection("jdbc:mysql://www.martinilink.co.uk:3306/doctor_app", "doctor_app", "JNpRFmbXk5WB68SW");
     }
 
+    private static class User {
+        private String email;
+
+        public User(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        @Override
+        public String toString() {
+            return email;
+        }
+
+    }
+
 }
