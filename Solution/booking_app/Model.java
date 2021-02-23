df9479d168c618d842db6579119505833a4be51d
Ben Martin <ben@brmartin.co.uk>
2021-02-23 14:00:09 +0000

    Create users getter

--- Solution/booking_app/Model.java
+++ Solution/booking_app/Model.java
@@ -2,12 +2,35 @@ package booking_app;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Model {
 
-    private Connection getConn() throws SQLException {
+    public static void main(String[] args) throws SQLException {
+        // Testing
+        Set<User> users = Model.getUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+
+    private static Connection getConn() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://www.martinilink.co.uk:3306/doctor_app", "doctor_app", "JNpRFmbXk5WB68SW");
+    }
+
+    public static Set<User> getUsers() throws SQLException {
+        try (Statement stmt = getConn().createStatement()) {
+            ResultSet result = stmt.executeQuery("SELECT `email` FROM `users`");
+            HashSet<User> users = new HashSet<>();
+            while (result.next()) {
+                users.add(new User(result.getString("email")));
+            }
+            return users;
+        }
     }
 
     private static class User {
