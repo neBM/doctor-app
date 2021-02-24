cc8746bbb571beaef846c7b343d052aceb753ce3
Hayden Rich <hr264@kent.ac.uk>
2021-02-24 19:03:37 +0000

    Added authentication check and open Interface

--- Solution/booking_app/LoginInterface.java
+++ Solution/booking_app/LoginInterface.java
@@ -4,21 +4,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Set;
 
-public class LoginInterface extends JFrame implements ActionListener {
+public class LoginInterface extends JFrame {
 
-    public static void main(String[] args){
+    private JFrame frame;
+    private JButton button;
+    private JPanel panelFooter;
+    private JPanel panelCenter;
+    private JPanel panelUsername;
+    private JPanel panelPassword;
+    private JLabel labelUsername;
+    private JLabel labelPassword;
+    private JLabel labelMessage;
+    private JTextField textUsername;
+    private JTextField textPassword;
+
+    public LoginInterface() {
         // Creating objects for Interface
-        JFrame frame = new JFrame();
-        JButton button = new JButton();
-        JPanel panelFooter = new JPanel();
-        JPanel panelCenter = new JPanel();
-        JPanel panelUsername = new JPanel();
-        JPanel panelPassword = new JPanel();
-        JLabel labelUsername = new JLabel();
-        JLabel labelPassword = new JLabel();
-        JTextField textUsername = new JTextField();
-        JTextField textPassword = new JTextField();
+        frame = new JFrame();
+        button = new JButton();
+        panelFooter = new JPanel();
+        panelCenter = new JPanel();
+        panelUsername = new JPanel();
+        panelPassword = new JPanel();
+        labelUsername = new JLabel();
+        labelPassword = new JLabel();
+        labelMessage = new JLabel();
+        textUsername = new JTextField();
+        textPassword = new JTextField();
 
         // Frame Information
         frame.setTitle("NHS Login Page");
@@ -32,6 +48,7 @@ public class LoginInterface extends JFrame implements ActionListener {
         // Label Information
         labelUsername.setText("Username");
         labelPassword.setText("Password");
+        labelMessage.setText("Hi");
 
         // Text Information
         textUsername.setPreferredSize(new Dimension(150, 20));
@@ -39,6 +56,7 @@ public class LoginInterface extends JFrame implements ActionListener {
 
         // Panel Information
         panelFooter.add(button);
+        panelFooter.add(labelMessage);
 
         panelUsername.add(labelUsername);
         panelUsername.setPreferredSize(new Dimension(250, 200));
@@ -54,11 +72,59 @@ public class LoginInterface extends JFrame implements ActionListener {
         // Login Button Information
         button.setText("Login");
         button.setLocation(125, 500);
+
+        // Confirmation Message Information
+        labelMessage.setLocation(150, 550);
+
+        // Button Action listener
+        
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                //if (e.getActionCommand().equals("button")) {
+                    String usernameField = textUsername.getText();
+                    String passwordField = textPassword.getText();
+                    try {
+                        Set<User> users = Model.getUsers();
+                        //checking if the username exists
+                        for (User user : users) {
+                            if(usernameField.contains(user.toString())){    
+                                try{
+                                    //checking they entered the correct password
+                                    if(user.testPassword(passwordField)){
+                                        labelMessage.setText("Successful log in attempt c:");
+                                        //TO-DO enter code to send them to the welcome screen on a successful password attempt
+                                        new Interface();
+                                        frame.setVisible(false);
+                                    }
+                                    else{
+                                        labelMessage.setText("The password was incorrect, please try again");
+                                        textPassword.setText("");
+                                    }
+                                }
+                                catch(NoSuchAlgorithmException e2){
+                                    e2.printStackTrace();
+                                }
+                            }
+                            else{
+                                labelMessage.setText("This username does not exist");
+                                textUsername.setText("");
+                                textPassword.setText("");
+                            }
+                        }
+                        }
+                     catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                    
+                //}
+            }
+        });
     }
 
-    public void actionPerformed(ActionEvent e){
-        if(e.getActionCommand().equals("button")){
-        }
+    public static void main(String[] args) {
+        LoginInterface logininterface = new LoginInterface();
+        
     }
 
+    
 }
