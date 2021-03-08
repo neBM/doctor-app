7700d25d72a236628798b4f3245eed9048640ea6
Sean Sanii Nejad <ss2439@kent.ac.uk>
2021-03-08 19:01:12 +0000

    Created JFrame & JPanel objects and set constructors for both

--- Solution/src/main/java/booking_app/BookingInterface.java
+++ Solution/src/main/java/booking_app/BookingInterface.java
@@ -1,1 +1,32 @@
+package booking_app;
+
+import javax.swing.*;
+
+public class BookingInterface {
+
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+
+    public BookingInterface(){
+        // Frame Information
+        frame.setLayout(null);
+        frame.setTitle("Booking Filter");
+        frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(375, 180);
+        frame.setResizable(false);
+        frame.add(panel);
+
+        // Panel Information
+        panel.setBounds(0, 0, 500, 200);
+        panel.setLayout(null);
+    }
+
+    public static void main (String[] args){
+        BookingInterface bookingInterface = new BookingInterface();
+    }
+}
+
+
+
 
