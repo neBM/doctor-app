bf43cc27cae2caa4005f7a17fdc02a436861d3dd
Sean Sanii Nejad <ss2439@kent.ac.uk>
2021-03-12 19:18:32 +0000

    Created JFrame and JPanel for ViewBookingInterface.java and set attributes

--- Solution/src/main/java/booking_app/ViewBookingInterface.java
+++ Solution/src/main/java/booking_app/ViewBookingInterface.java
@@ -3,6 +3,28 @@ package booking_app;
 import javax.swing.*;
 
 public class ViewBookingInterface {
+    // JFrame Object
+    private JFrame frame = new JFrame();
 
-    
+    // JPanel Object
+    private JPanel panel = new JPanel();
+
+    ViewBookingInterface(){
+        // Frame Information
+        frame.setLayout(null);
+        frame.setTitle("Past Bookings");
+        frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(600, 400);
+        frame.setResizable(false);
+        frame.add(panel);
+
+        // Panel Information
+        panel.setLayout(null);
+        panel.setBounds(0, 0, 600, 400);
+    }
+
+    public static void main(String[] args){
+        ViewBookingInterface main = new ViewBookingInterface();
+    }
 }
